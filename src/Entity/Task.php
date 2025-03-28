b6ebd69f97dc0604289a1181b49bfb517193c721
yassine <y31m3x@gmail.com>
2025-03-28 15:49:21 +0100

    partie n°1

---
+++ src/Entity/Task.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+
+
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank(
+        min: 2,
+        message:"Le titre doit faire au moins 2 caractères !"
+    )]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(
+        message:"Le contenu de la tache ne peux pas être vide !"
+    )]
+    private ?string $content = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $experedAt = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getExperedAt(): ?\DateTimeImmutable
+    {
+        return $this->experedAt;
+    }
+
+    public function setExperedAt(?\DateTimeImmutable $experedAt): static
+    {
+        $this->experedAt = $experedAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
