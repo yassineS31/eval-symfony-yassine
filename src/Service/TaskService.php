6cdad19918b6b31daddd7ef1080871e5abd4a549
yassine <y31m3x@gmail.com>
2025-03-28 16:13:01 +0100

    partie n°2

---
+++ src/Service/TaskService.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Service;
+
+use PhpParser\Node\Stmt\TryCatch;
+use function PHPUnit\Framework\throwException;
+class TaskService{
+
+    // Ajout d'une tache
+public function addTask(){
+    try {
+        
+    } catch (\Throwable $th) {
+        //throw $th;
+    }
+}
+//Afficher toutes les taches
+public function GetAllTasks(){
+    try {
+        //code...
+    } catch (\Throwable $th) {
+    }
+}
+}
