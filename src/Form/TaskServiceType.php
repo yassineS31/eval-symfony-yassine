6adaddfe7f4f191c6128e6bf50c391c6e1e31c4f
yassine <y31m3x@gmail.com>
2025-03-28 16:16:45 +0100

    partie n°3

---
+++ src/Form/TaskServiceType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskServiceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('experedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('status',false)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
